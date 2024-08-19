a07e13657cf7b5904ebf51cb31bdf4f28e8eb945
劉奕劭 <72032c@gmail.com>
2024-08-19 12:45:11 +0000

    202408192045

--- 7_0819/7-1.py
+++ 7_0819/7-1.py
@@ -1,19 +1,23 @@
 import random
 import pyinputplus as pyip
-min=1
-max=100
-count=0
-ans=random.randint(min,max+1)
-print("======猜數字遊戲======")
 while True:
-    count+=1
-    keyin=pyip.inputInt(f"請猜數字,範圍:{min}~{max}\n",min=min,max=max)
-    if keyin==ans:
-        print(f"您猜對了! 答案為{ans}")
-        print(f"您猜了{count}次")
+    min=1
+    max=100
+    count=0
+    ans=random.randint(min,max+1)
+    print("======猜數字遊戲======")
+    while True:
+        count+=1
+        keyin=pyip.inputInt(f"請猜數字,範圍:{min}~{max}\n",min=min,max=max)
+        if keyin==ans:
+            print(f"您猜對了! 答案為{ans}")
+            print(f"您猜了{count}次")
+            break
+        elif keyin>ans:
+            max=keyin-1
+        elif keyin<ans:
+            min=keyin+1
+    yesno=pyip.inputYesNo("您還要繼續猜數字嗎?(y or n)")
+    if yesno=="no":
         break
-    elif keyin>ans:
-        max=keyin-1
-    elif keyin<ans:
-        min=keyin+1
 print("應用程式結束")
