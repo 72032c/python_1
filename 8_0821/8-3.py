aedda21a0f93bb8cf424f68b9e709b7d87058e9e
劉奕劭 <72032c@gmail.com>
2024-08-21 12:39:42 +0000

    202408212039

---
+++ 8_0821/8-3.py
@@ -0,0 +1,16 @@
+from widget import tools
+
+while True:
+    name=str(input("請輸入姓名:(輸入q再按enter離開)"))
+    if name=='q':
+        break
+    try:
+        height=float(input('請輸入身高:'))
+        weight=float(input('請輸入體重:'))
+        bmi=round(weight/(height*0.01)**2,ndigits=2)
+        grade=tools.get_status_message(bmi)
+        print(f"{name}的BMI為{bmi:.2f}\n體重:{grade}")
+    except ValueError:
+        print("格式錯誤")
+
+print("應用程式結束")
--- 8_0821/widget/tools.py
+++ 8_0821/widget/tools.py
@@ -1,13 +1,13 @@
 def get_status_message(bmi: float) -> str:
-    if bmi < 18.5:
-        return "體重過輕"
-    elif bmi < 24:
-        return "正常範圍"
-    elif bmi < 27:
-        return "過重"
-    elif bmi < 30:
-        return "輕度肥胖"
-    elif bmi < 35:
-        return "中度肥胖"
-    else:
-        return "重度肥胖"
+    if bmi<18.5:
+        grade="過輕"
+    elif bmi<24:
+        grade="正常"
+    elif bmi<27:
+        grade="過重"
+    elif bmi<30:
+        grade="輕度肥胖"
+    elif bmi<35:
+        grade="中度肥胖"
+    elif bmi>=35:
+        grade="重度肥胖"
