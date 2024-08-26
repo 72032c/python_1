c5235849dea5f3b70de7fed3bb9c50233427383f
劉奕劭 <72032c@gmail.com>
2024-08-26 11:23:30 +0000

    202408261923

--- 8_0821/8-homework.py
+++ 8_0821/8-homework.py
@@ -1,20 +1,18 @@
-#請自訂一個class叫BMI
-
 class BMI():
-    def __init__(self,name:str,height:float,weight:float):
-        self.name=name
-        self.height=height
-        self.weight=weight
+    def __init__(self, name:str, height:float, weight:float):
+        self.name = name
+        self.height = height
+        self.weight = weight
 
-    def __init__(self)->str:
+    def getName(self)->str:
         return self.name
     
-    def __init__(self)->float:
-        return round(self.while/((self.height*0.01)**2),2)
-
-    def __init__(self)->str:
-        self.bmi=self.getbmi()
-            
+    def getBMI(self)->float:
+        return round(self.weight / ((self.height * 0.01) ** 2), ndigits=2)
+    
+    def get_status_message(self)->str:
+        self.bmi = self.getBMI()
+        
         if self.bmi < 18.5:
             return "體重過輕"
         elif self.bmi < 24:
@@ -33,11 +31,10 @@ while True:
         name = input("請輸入姓名: ")
         height = float(input('請輸入身高（cm）: '))
         weight = float(input('請輸入體重（kg）: '))
-
+        
         #這裏建立一個BMI的實體
-        bmi=bmi(name=name,height=height,weight=weight)
-        print(f"您:{bmi.getName()}\nBMI為{bmi.getBMI()}\n體重為{bmi.get_status_message()}")
-
+        MyBMI = BMI(name=name, height=height, weight=weight)
+        print(f"{MyBMI.getName()} 的 BMI 為{MyBMI.getBMI()}, 為{MyBMI.get_status_message()}")
     except ValueError:
         print("格式錯誤，請重新輸入數據")
         continue
